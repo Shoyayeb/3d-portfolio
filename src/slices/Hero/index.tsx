995cc3f095d01b603a683f06f85d0baa09598883
Soyayeb Hasan Shafin <sa.fine@outlook.com>
2024-08-09 17:37:58 +0600

    hero shapes

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,7 +4,8 @@ import { useEffect, useRef } from "react";
 import Bounded from "@/components/Bounded";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import Shapes from './Shapes';
 /**
  * Props for `Hero`.
  */
@@ -67,6 +68,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid m-h-[70vh] grid-cols-1 xl:grid-cols-2 items-center">
+      <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-[clamp(3rem,18vmin,18rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
             <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
@@ -76,6 +78,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <span className="job-title block bg-gradient-to-tr from-yellow-500 via-blue-400 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
           </h1>
         </div>
+        
       </div>
     </Bounded>
   );
